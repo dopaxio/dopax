88d7338adf5c4ca7bbd50e5631863df72c629a0a
uu <tensorstaff@gmail.com>
2026-04-21 15:47:16 +0800

    Revert "Add desktop agent packaging and CLI discovery"
    This reverts commit 2a726ad1fde4d904ce0582e8e3ed399dd96e1cbc.

--- matrix-bot-ts/scripts/start-supervisor-demo-remote.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-set -eu
-
-ROOT="$(cd "$(dirname "$0")/.." && pwd)"
-CONFIG="${1:-$ROOT/supervisor/remote.example.json}"
-
-cd "$ROOT"
-npm run build
-exec npm run supervisor:start -- "$CONFIG"
--- matrix-bot-ts/scripts/start-supervisor-demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-set -eu
-
-ROOT="$(cd "$(dirname "$0")/.." && pwd)"
-CONFIG="${1:-$ROOT/supervisor/local.example.json}"
-
-cd "$ROOT"
-npm run build
-exec npm run supervisor:start -- "$CONFIG"
